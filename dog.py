e16825b2ded762e20ec26e97dfa02a1033fe4ace
Steve Hong <steveachu@users.noreply.github.com>
2016-12-09 14:38:19 -0800

    Chapter 9 of Python Crash Course
    A simple class example modeling a dog.

---
+++ dog.py
@@ -0,0 +1,30 @@
+class Dog():
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(self.name.title() + " rolled over!")
+
+my_dog = Dog('skip', 6)
+your_dog = Dog('ralph', 10)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+
+print("Your dog's name is " + your_dog.name.title() + ".")
+print("Your dog is " + str(your_dog.age) + " years old.")
+
+my_dog.sit()
+my_dog.roll_over()
+
+your_dog.sit()
+your_dog.roll_over()
