fdbe508d2b72ae891244ddee5578cdd43e338de4
Steve Hong <steveachu@users.noreply.github.com>
2016-12-09 23:56:30 -0800

    Python Crash Course example from Chapter 9
    Demonstrates an ElectricCar class that inherits from a Car class.

---
+++ inheritance.py
@@ -0,0 +1,34 @@
+class Car():
+    """A simple model of a car."""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        if mileage <= self.odometer_reading:
+            print("You can't roll back the odometer.")
+        else:
+            self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class."""
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
