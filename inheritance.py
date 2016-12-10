9e751c3f961e84b94e3c44ee1db432daea7d19ab
Steve Hong <steveachu@users.noreply.github.com>
2016-12-10 00:05:37 -0800

    Added Battery class.
    Added a separate class to represent an electric car Battery.

--- inheritance.py
+++ inheritance.py
@@ -29,12 +29,21 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
-        self.battery_size = 70
+        self.battery = Battery()
 
     def describe_battery(self):
         """Print a statement describing the battery size."""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
+class Battery():
+    def __init__(self, battery_size=70):
+        """Initialize battery attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a satement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
