9b0cc1f028eae2d48fffaf559dd1dbfcbe73d91a
Steve Hong <steveachu@users.noreply.github.com>
2016-12-09 23:59:56 -0800

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -29,6 +29,12 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class."""
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
